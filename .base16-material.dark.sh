37d9649562a089a9447904dcb7d8f479147dcc70
Maximilian Antoni <mail@maxantoni.de>
2016-02-23 11:41:28 +0100

    Make base16-material.dark.sh work on linux

--- .base16-material.dark.sh
+++ .base16-material.dark.sh
@@ -2,121 +2,53 @@
 # Base16 material - Shell color setup script
 # Sean Washington (http://seanwash.com)
 
-if [ "${TERM%%-*}" = 'linux' ]; then
-    # This script doesn't support linux console (use 'vconsole' template instead)
-    return 2>/dev/null || exit 0
-fi
-
-color00="26/32/38" # Base 00 - Black
-color01="cc/66/66" # Base 08 - Red
-color02="b5/bd/68" # Base 0B - Green
-color03="f0/c6/74" # Base 0A - Yellow
-color04="81/a2/be" # Base 0D - Blue
-color05="b2/94/bb" # Base 0E - Magenta
-color06="8a/be/b7" # Base 0C - Cyan
-color07="ec/ef/ef" # Base 05 - White
-color08="70/78/80" # Base 03 - Bright Black
+color00="263238" # Base 00 - Black
+color01="cc6666" # Base 08 - Red
+color02="b5bd68" # Base 0B - Green
+color03="f0c674" # Base 0A - Yellow
+color04="81a2be" # Base 0D - Blue
+color05="b294bb" # Base 0E - Magenta
+color06="8abeb7" # Base 0C - Cyan
+color07="ecefef" # Base 05 - White
+color08="707880" # Base 03 - Bright Black
 color09=$color01 # Base 08 - Bright Red
 color10=$color02 # Base 0B - Bright Green
 color11=$color03 # Base 0A - Bright Yellow
 color12=$color04 # Base 0D - Bright Blue
 color13=$color05 # Base 0E - Bright Magenta
 color14=$color06 # Base 0C - Bright Cyan
-color15="ff/ff/ff" # Base 07 - Bright White
-color16="cc/66/66" # Base 09
-color17="cc/66/66" # Base 0F
-color18="26/32/38" # Base 01
-color19="37/47/4f" # Base 02
-color20="b5/bd/68" # Base 04
-color21="ec/ef/ef" # Base 06
-color_foreground="ec/ef/ef" # Base 05
-color_background="26/32/38" # Base 00
-color_cursor="ec/ef/ef" # Base 05
-
-if [ -n "$TMUX" ]; then
-  # tell tmux to pass the escape sequences through
-  # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
-  printf_template="\033Ptmux;\033\033]4;%d;rgb:%s\007\033\\"
-  printf_template_var="\033Ptmux;\033\033]%d;rgb:%s\007\033\\"
-  printf_template_custom="\033Ptmux;\033\033]%s%s\007\033\\"
-elif [ "${TERM%%-*}" = "screen" ]; then
-  # GNU screen (screen, screen-256color, screen-256color-bce)
-  printf_template="\033P\033]4;%d;rgb:%s\007\033\\"
-  printf_template_var="\033P\033]%d;rgb:%s\007\033\\"
-  printf_template_custom="\033P\033]%s%s\007\033\\"
-else
-  printf_template="\033]4;%d;rgb:%s\033\\"
-  printf_template_var="\033]%d;rgb:%s\033\\"
-  printf_template_custom="\033]%s%s\033\\"
-fi
+color15="ffffff" # Base 07 - Bright White
+color16="cc6666" # Base 09
+color17="cc6666" # Base 0F
+color18="263238" # Base 01
+color19="37474f" # Base 02
+color20="b5bd68" # Base 04
+color21="ecefef" # Base 06
 
 # 16 color space
-printf $printf_template 0  $color00
-printf $printf_template 1  $color01
-printf $printf_template 2  $color02
-printf $printf_template 3  $color03
-printf $printf_template 4  $color04
-printf $printf_template 5  $color05
-printf $printf_template 6  $color06
-printf $printf_template 7  $color07
-printf $printf_template 8  $color08
-printf $printf_template 9  $color09
-printf $printf_template 10 $color10
-printf $printf_template 11 $color11
-printf $printf_template 12 $color12
-printf $printf_template 13 $color13
-printf $printf_template 14 $color14
-printf $printf_template 15 $color15
+printf "\e]P0$color00\e\\"
+printf "\e]P1$color01\e\\"
+printf "\e]P2$color02\e\\"
+printf "\e]P3$color03\e\\"
+printf "\e]P4$color04\e\\"
+printf "\e]P5$color05\e\\"
+printf "\e]P6$color06\e\\"
+printf "\e]P7$color07\e\\"
+printf "\e]P8$color08\e\\"
+printf "\e]P9$color09\e\\"
+printf "\e]PA$color10\e\\"
+printf "\e]PB$color11\e\\"
+printf "\e]PC$color12\e\\"
+printf "\e]PD$color13\e\\"
+printf "\e]PE$color14\e\\"
+printf "\e]PF$color15\e\\"
 
 # 256 color space
-printf $printf_template 16 $color16
-printf $printf_template 17 $color17
-printf $printf_template 18 $color18
-printf $printf_template 19 $color19
-printf $printf_template 20 $color20
-printf $printf_template 21 $color21
-
-# foreground / background / cursor color
-if [ -n "$ITERM_SESSION_ID" ]; then
-  # iTerm2 proprietary escape codes
-  printf $printf_template_custom Pg ecefef # forground
-  printf $printf_template_custom Ph 1b1b1b # background
-  printf $printf_template_custom Pi ecefef # bold color
-  printf $printf_template_custom Pj 37474f # selection color
-  printf $printf_template_custom Pk ecefef # selected text color
-  printf $printf_template_custom Pl ecefef # cursor
-  printf $printf_template_custom Pm 263238 # cursor text
-else
-  printf $printf_template_var 10 $color_foreground
-  printf $printf_template_var 11 $color_background
-  printf $printf_template_custom 12 ";7" # cursor (reverse video)
+if [ "$TERM" != linux ]; then
+  printf "\e]4;16;rgb:$color16\e\\"
+  printf "\e]4;17;rgb:$color17\e\\"
+  printf "\e]4;18;rgb:$color18\e\\"
+  printf "\e]4;19;rgb:$color19\e\\"
+  printf "\e]4;20;rgb:$color20\e\\"
+  printf "\e]4;21;rgb:$color21\e\\"
 fi
-
-# clean up
-unset printf_template
-unset printf_template_var
-unset color00
-unset color01
-unset color02
-unset color03
-unset color04
-unset color05
-unset color06
-unset color07
-unset color08
-unset color09
-unset color10
-unset color11
-unset color12
-unset color13
-unset color14
-unset color15
-unset color16
-unset color17
-unset color18
-unset color19
-unset color20
-unset color21
-unset color_foreground
-unset color_background
-unset color_cursor
