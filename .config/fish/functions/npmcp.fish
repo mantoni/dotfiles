719e37de46ad7045cd203085c329ebc825cda635
Maximilian Antoni <mail@maxantoni.de>
2016-05-12 09:21:44 +0200

    Fix fish scripts

--- .config/fish/functions/npmcp.fish
+++ .config/fish/functions/npmcp.fish
@@ -1,11 +1,11 @@
 function npmcp
   set -l npm_prefix (npm config get prefix)
-  set -l source "$npm_prefix/lib/node_modules/$1"
+  set -l source "$npm_prefix/lib/node_modules/$argv[1]"
   if [ ! -e $source ]
     echo "$source not found"
     return 1
   end
-  set -l target "node_modules/$1"
+  set -l target "node_modules/$argv[1]"
   rm -rf $target
   mkdir -p $target
   set -l files (ls $source | grep -v node_modules)
