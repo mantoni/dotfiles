e270b1ded565b22153929c41e152efdfcc21bf3b
Maximilian Antoni <mail@maxantoni.de>
2017-03-28 13:39:49 +0200

    Remove old `changes` script

--- .config/fish/functions/changes.fish
+++
@@ -1,4 +0,0 @@
-function changes
-  set -l package_version (node -p "require('./package.json').version")
-  git log v$package_version..HEAD --format="- %s (%an)" --reverse | sed -e 's/ (Maximilian Antoni)//g'
-end
