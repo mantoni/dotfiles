b9e732f9aa299891a09030c2cca26765475a8e11
Maximilian Antoni <mail@maxantoni.de>
2021-01-26 11:10:14 +0100

    Use node 14

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,7 +25,7 @@ set -gx fish_color_command purple
 set -gx fish_color_param cyan
 set -gx fish_color_selection --background=263238
 set -gx fish_color_search_match --background=263238
-# set -g fish_user_paths "/usr/local/bin" "/usr/local/opt/node@8/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/bin" "/usr/local/opt/node@14/bin" $fish_user_paths
 
 alias l 'ls -lh'
 alias ll 'ls -alh'
