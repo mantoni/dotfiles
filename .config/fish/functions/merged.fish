9c388394f9bbd704402ea7b4b69a98e972ec9f55
Maximilian Antoni <mail@maxantoni.de>
2021-06-09 10:03:32 +0200

    Add `merged` script

---
+++ .config/fish/functions/merged.fish
@@ -0,0 +1,29 @@
+function merged
+  set -l branch $argv[1]
+
+  if [ "$branch" != 'main' -a "$branch" != 'master' ]
+    echo -n "Require branch argument "
+    set_color red
+    echo -n "main"
+    set_color normal
+    echo -n " or "
+    set_color red
+    echo -n "master"
+    set_color normal
+    echo
+    return 1
+  end
+
+  set -l pr_branch (git branch --show-current)
+  if [ $pr_branch = $branch ]
+    echo -n "Current branch is "
+    set_color red
+    echo -n $branch
+    set_color normal
+    echo
+    return 1
+  end
+
+  git checkout main
+  git branch -D $pr_branch
+end
