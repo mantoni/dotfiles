0ab3733248b0ffe48926363879c8a189ae63f4a5
Maximilian Antoni <mail@maxantoni.de>
2024-03-11 16:53:11 +0100

    Require confirmation on "g co -- ."

--- .config/fish/functions/g.fish
+++ .config/fish/functions/g.fish
@@ -1,5 +1,13 @@
 function g
   if count $argv > /dev/null
+    if test "$argv[1]" = 'checkout' || test "$argv[1]" = 'co'
+      if begin test "$argv[2]" = '.'; or test "$argv[2]" = '--' && test "$argv[3]" = '.'; end
+        read -l -P 'Discard all changes? [y/N] ' confirm
+        if test $confirm = '' || test $confirm = 'n' || test $confirm = 'N'
+          return 1
+        end
+      end
+    end
     git $argv
   else
     git status --short --branch
